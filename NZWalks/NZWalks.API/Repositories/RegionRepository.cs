b220d5d789fc9b7799d623587967abf11b874055
DO04dev <thaboroy@gmail.com>
2023-02-25 01:54:44 +0200

    Using DTOs or Contracts

---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext nZWalksDBContext;
+
+        public RegionRepository(NZWalksDBContext nZWalksDBContext)
+        { 
+            this.nZWalksDBContext = nZWalksDBContext;
+        }
+
+        public IEnumerable<Region> GetAll()
+        {
+            return nZWalksDBContext.Regions.ToList();
+        }
+    }
+}
