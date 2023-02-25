f0ee8a5d2b1b8a59fdebd3c17528feafdc88c529
DO04dev <thaboroy@gmail.com>
2023-02-25 14:23:18 +0200

    Install and use automapper

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDbContext<NZWalksDBContext>(options =>
 });
 
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
